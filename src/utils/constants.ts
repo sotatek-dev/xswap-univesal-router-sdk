bf4c00f91c026ed0cc4bf5847ab2d2544733ab84
Sotatek-PhuongNguyen4 <phuong.nguyen4@sotatek.com>
2023-03-30 15:59:22 +0700

    edit goerli contract

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
     case 1: // mainnet
       return '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B'
     case 5: // goerli
-      return '0x4648a43B2C14Da09FdF82B161150d3F634f40491'
+      return '0x27Daf3f78fCdE43E40E4F3F144FEbc856f7352F8'
     case 137: // polygon
       return '0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5'
     case 80001: // polygon mumbai
